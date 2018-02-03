aa9a69778ecb92d3c74c7a18038f349da0291747
Olivia Orr <oorr90@gmail.com>
2018-02-03 17:14:49 -0500

    Creating front-page and index page

--- index.php
+++ index.php
@@ -18,26 +18,18 @@
 
  <!-- MAIN -->
     
-<main>
-    <div class="home-name">
-        <h1><span>O</span>livia<br><span>H</span>eshima<br><span>O</span>rr</h1>
-    </div>
-    <div class="home-links">
-        <section class="welcome home-block">
-            <h2>Welcome</h2>
-            <p>Hi, I'm Olivia and I like to <a href="#">create things</a>. I'm particularly passionate about social justice and projects that impact people in my community. </p>
-        </section>
-
-        <section class="introduction home-block">
-            <h2>Introduction</h2>
-            <p>I am based in Portland, Maine. When I'm not on my grind at my 9:00 - 5:00 job, I continuously push to improve my <a href="#">programming and design</a> skills.</p>
-        </section>
-
-        <section class="contact home-block">
-            <h2>Say Hi</h2>
-            <p>Feel free to <a href="#">contact me</a> or take a look around my site to learn more.</p>
-        </section>
-    </div>
+<main class="main-page">
+    
+    <?php
+        if (have_posts()):
+          while (have_posts()) : the_post();
+            the_content();
+          endwhile;
+        else:
+          echo '<p>Sorry, no posts matched your criteria.</p>';
+        endif;
+    ?>
+    
 </main>
     
 <?php echo get_footer(); ?>
