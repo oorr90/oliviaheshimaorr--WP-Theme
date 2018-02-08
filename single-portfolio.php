0607e2bdca8ea7e321b86c6fb56e2deb15ffae22
Olivia Orr <oorr90@gmail.com>
2018-02-07 22:30:08 -0500

    Adding single portfolio page

---
+++ single-portfolio.php
@@ -0,0 +1,33 @@
+<?php
+
+ echo get_header();
+
+ ?>
+
+
+ <!-- MAIN -->
+    
+<main class="main-page">
+
+
+    <!-- BEGIN INNER WRAPPER -->
+    <div <?php 
+            if ( is_page('contact')) { 
+		      echo 'class="contact-page-wrap"'; 
+	       } ?> >
+ 
+     <?php
+        
+        the_title();
+
+        ?>
+     
+    
+    
+    <!-- END INNER WRAPPER -->
+    </div>
+
+    
+</main>
+    
+<?php echo get_footer(); ?>
